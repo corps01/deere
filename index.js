a1298b0bffb071cb36f05ebe44c1355614a8fb55
corps01 <ocorpus1@outlook.com>
2024-07-01 17:58:59 -0600

    add html

--- index.js
+++ index.js
@@ -18,8 +18,19 @@ app.get('/user/:id', (req, res) => {
 // POST endpoint
 app.post('/notify', (req, res) => {
     const data = req.body;
-    const { job_id, workspace_name, description, datetime, change, summarizer } = data;
-    notifyResponse = `Job ID: ${job_id}\nWorkspace Name: ${workspace_name}\nDescription: ${description}\nDate Time: ${datetime}\nChange: ${change}\nSummary: ${summarizer}`;
+    const { job_id, workspace_name, description, datetime, change, summarizer, added_text, removed_text } = data;
+
+    notifyResponse = `
+    <p><b>Job ID:</b> ${job_id}</p>
+    <p><b>Workspace Name:</b> ${workspace_name}</p>
+    <p><b>Description:</b> ${description}</p>
+    <p><b>Date Time:</b> ${datetime}</p>
+    <p><b>Change:</b> ${change}</p>
+    <p><b>Added Text:</b> ${added_text}</p>
+    <p><b>Removed Text:</b> ${removed_text}</p>
+    <p><b>Summary:</b> ${summarizer}</p>
+    `;
+
     res.send(notifyResponse);
     console.log(`data received: ${notifyResponse}`)
 });
